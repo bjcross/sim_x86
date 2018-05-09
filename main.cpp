87ffef90c0a03b4b07c16cab756676750f92af72
brandon <bjcross@umass.edu>
2018-05-09 04:39:54 -0400

    hmmm

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
-#include <iostream>
+#include <stdio.h>			//standard also allows fopen
+#include <stdlib.h>
 
+#include <iostream>			//allow streaming
+using namespace std;
 int main(int argc, char *argv[]){
    FILE* input = NULL; 		//pointer to the file that we will be using to input x86 code
    FILE* output = NULL;		//pointer to the file we will write to with memory information
 
-	if (argc == 2){ 			//check for the correct number of arguments (2) input/output files
+	if (argc == 3){ 			//check for the correct number of arguments (2) input/output files
 		input = fopen(argv[0], "r");
 		output = fopen(argv[1], "w");		
 	}
 	else {
-		cout << "Please enter (INPUT OUTPUT) and nothing more";
+		cout << "Please enter (INPUT OUTPUT) and nothing more" << endl;
 		exit(0);
 	}
 
 	if (input == NULL || output == NULL){	//check that the inpute files exist
-		cout << "Atleast one of the input files doesn't exist";	
+		cout << "Atleast one of the input files doesn't exist" << endl;
 		exit(0);
 	}
 
@@ -32,10 +35,18 @@ int main(int argc, char *argv[]){
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~fix input for common errors~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	char* fixedcommand;
+
+	while (!feof(input)){
+		fgets(fixedcommand, 100, input);
+		cout << "Commands: " << fixedcommand << endl;	
 
 
 
-	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~convert reg names to reg numbers~~~~~~~~~~~~~~~~~~~~~~~~
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~convert reg names to reg numbers~~~~~~~~~~~~~~~~~~~~~~~~
 
-	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~store this code to instruction memory~~~~~~~~~~~~~~~~~~
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~store this code to instruction memory~~~~~~~~~~~~~~~~~~
+
+
+	}
 }
