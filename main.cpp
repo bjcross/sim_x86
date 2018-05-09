a681fca54b51e43e683130a4933a4bf5d77136fe
brandon <bjcross@umass.edu>
2018-05-09 05:15:45 -0400

    setting up enum

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <stdio.h>			//standard also allows fopen
 #include <stdlib.h>
-
+#include <algorithm>
+#include <string>
 #include <iostream>			//allow streaming
 using namespace std;
+
+enum regnum {ax = 0, bx, cx, dx, si, di, bp, sp, 8, 9 , 10, 11, 12, 13, 14, 15};
+
+
+
+
 int main(int argc, char *argv[]){
    FILE* input = NULL; 		//pointer to the file that we will be using to input x86 code
    FILE* output = NULL;		//pointer to the file we will write to with memory information
 
 	if (argc == 3){ 			//check for the correct number of arguments (2) input/output files
-
-		cout << argv[0] << " : " << argv[1] << endl;
 		input = fopen(argv[1], "r");
 		output = fopen(argv[2], "w");		
 	}
@@ -38,13 +43,19 @@ int main(int argc, char *argv[]){
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~fix input for common errors~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	char fixedcommand[100];
 
-	while(fgets(fixedcommand, 100, input)){
-		cout << "Commands: " << fixedcommand << endl;	
-
-
+	while(fgets(fixedcommand, 100, input)){	//puts the line from file to fixedcommand one by one
+		for(int i=0; i<sizeof(fixedcommand); i++){
+			fixedcommand[i] = tolower(fixedcommand[i]);
+		}
+		
 
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~convert reg names to reg numbers~~~~~~~~~~~~~~~~~~~~~~~~
-
+		for(int i=0; i<sizeof(fixedcommand); i++){
+			if(fixedcommand[i] == '%'){
+				i++;
+			
+			}
+		}	
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~store this code to instruction memory~~~~~~~~~~~~~~~~~~
 
 
